b258aac97e3cb7dcce43f4170a4df8fa43ba9aff
FranChesK0 <khl_doss@mail.ru>
2024-10-12 02:24:20 +0800

    update auth page

--- simple-chat-app/static/js/auth.js
+++ simple-chat-app/static/js/auth.js
@@ -1,21 +1,17 @@
-// Обработка кликов по вкладкам
-document.querySelectorAll('.tab').forEach(tab => {
-    tab.addEventListener('click', () => showTab(tab.dataset.tab));
-});
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
 
-// Функция отображения выбранной вкладки
-function showTab(tabName) {
-    document.querySelectorAll('.tab').forEach(tab => tab.classList.remove('active'));
-    document.querySelectorAll('.form').forEach(form => form.classList.remove('active'));
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+})
 
-    document.querySelector(`.tab[data-tab="${tabName}"]`).classList.add('active');
-    document.getElementById(`${tabName}Form`).classList.add('active');
-}
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+})
 
-// Функция для валидации данных формы
 const validateForm = fields => fields.every(field => field.trim() !== '');
 
-// Функция для отправки запросов
 const sendRequest = async (url, data) => {
     try {
         const response = await fetch(url, {
@@ -27,55 +23,52 @@ const sendRequest = async (url, data) => {
         const result = await response.json();
 
         if (response.ok) {
-            alert(result.message || 'Операция выполнена успешно!');
+            alert(result.message || 'Operation successful');
             return result;
         } else {
-            alert(result.message || 'Ошибка выполнения запроса!');
+            alert(result.message || 'Request error');
             return null;
         }
     } catch (error) {
-        console.error("Ошибка:", error);
-        alert('Произошла ошибка на сервере');
+        console.error("Error:", error);
+        alert('Internal server error');
     }
 };
 
-// Функция для обработки формы
 const handleFormSubmit = async (formType, url, fields) => {
     if (!validateForm(fields)) {
-        alert('Пожалуйста, заполните все поля.');
+        alert("Please, enter all fields");
         return;
     }
 
     const data = await sendRequest(url, formType === 'login'
         ? {email: fields[0], password: fields[1]}
-        : {email: fields[0], name: fields[1], password: fields[2], password_check: fields[3]});
-
+        : {email: fields[0], name: fields[1], password: fields[2], password_check: fields[3]}
+    );
     if (data && formType === 'login') {
         window.location.href = '/chat';
     }
 };
 
-// Обработка формы входа
-document.getElementById('loginButton').addEventListener('click', async (event) => {
+document.getElementById('login-button').addEventListener('click', async (event) => {
     event.preventDefault();
 
-    const email = document.querySelector('#loginForm input[type="email"]').value;
-    const password = document.querySelector('#loginForm input[type="password"]').value;
+    const email = document.querySelector('#login-form input[type="email"').value;
+    const password = document.querySelector('#login-form input[type="password"').value;
 
     await handleFormSubmit('login', 'login/', [email, password]);
 });
 
-// Обработка формы регистрации
-document.getElementById('registerButton').addEventListener('click', async (event) => {
+document.getElementById('register-button').addEventListener('click', async (event) => {
     event.preventDefault();
 
-    const email = document.querySelector('#registerForm input[type="email"]').value;
-    const name = document.querySelector('#registerForm input[type="text"]').value;
-    const password = document.querySelectorAll('#registerForm input[type="password"]')[0].value;
-    const password_check = document.querySelectorAll('#registerForm input[type="password"]')[1].value;
+    const email = document.querySelector('#register-form input[type="email"]').value;
+    const name = document.querySelector('#register-form input[type="text"]').value;
+    const password = document.querySelector('#register-form input[type="password"]')[0].value;
+    const password_check = document.querySelector('#register-form input[type="password"]')[1].value;
 
     if (password !== password_check) {
-        alert('Пароли не совпадают.');
+        alert('Passwords mismatched');
         return;
     }
 
