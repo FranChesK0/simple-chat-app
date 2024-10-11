74a5905f66a05ea0950d7005a7d4fd54afa2c08e
FranChesK0 <khl_doss@mail.ru>
2024-10-11 14:34:13 +0800

    update alembic migration

--- simple-chat-app/database/migration/env.py
+++ simple-chat-app/database/migration/env.py
@@ -1,4 +1,6 @@
+import sys
 import asyncio
+from os.path import abspath, dirname
 from logging.config import fileConfig
 
 from alembic import context
@@ -6,9 +8,15 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
+from chat import Message  # noqa: F401
+from user import User  # noqa: F401
+from database import DB_URL, Base
+
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", DB_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -19,7 +27,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
